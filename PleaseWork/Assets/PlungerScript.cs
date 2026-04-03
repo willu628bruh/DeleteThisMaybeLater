53cf907d4018614c67785e1314adcb188382c5d6
Memesicles <christiancorona047@gmail.com>
2026-04-03 11:29:53 -0400

    plunger code

--- PleaseWork/Assets/PlungerScript.cs
+++ PleaseWork/Assets/PlungerScript.cs
@@ -4,24 +4,61 @@ using UnityEngine.UI;
 public class PlungerScript : MonoBehaviour
 {
     float power;
+    float minPower = 0f;
     public float maxPower = 100f;
     public Slider powerSlider;
+    List<Rigidbody> ballsList;
+    bool ballReady;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         powerSlider.minValue = 0f;
         powerSlider.maxValue = maxPower;
+        ballList = new List<Rigidbody>();
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(ballReady){
+            powerSlider.gameObject.SetActive(true);
+        } else {
+            powerSlider.gameObject.SetActive(false);
+        }
         powerSlider.value = power;
+        if(ballList.Count > 0){
+            ballReady = true;
+            if(Input.GetKey(KeyCode.Space)){
+                if(power <= maxPower){
+                    power+=50*Time.deltaTime;
+                }
+            }
+
+            if(Input.GetKeyUp(KeyCode.Space)){
+                if(power <= maxPower){
+                    foreach(Rigidbody r in ballsList){
+                        r.AddForce(power*Vector3.forward);
+                    }
+                }
+            } else {
+                ballReady = false;
+                power = 0f;
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")) ;
+        if (other.gameObject.CompareTag("Player")){
+            ballList.Add(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+    private void OnTriggerExit(Collider other){
+        if (other.gameObject.CompareTag("Player")){
+            ballsList.Remove(other.gameObject.GetComponent<Rigidbody>());
+            power = 0f;
+        }
     }
 
 }
